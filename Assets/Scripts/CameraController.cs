771f95764520cd03297acbb503594f99a3b8bb28
Ralf <danij9112@gmail.com>
2025-01-22 23:09:09 +0900

    Feat : 플레이어 캐릭터 변경
    - 캐릭터 변경
    - 에니메이션 추가
    - 카메라 이동 방식 수정

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,7 +9,6 @@ public class CameraController : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 desiredPosition = player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        transform.position = desiredPosition;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,60 @@
+using Unity.Mathematics;
+using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public AnimatorController animatorController;
+    public Animator animator;
+    public Transform characterTransform;
+
+
     private Rigidbody2D _rigidbody;
     private Vector2 _movement;
+    private bool _isRight;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void Flip()
     {
-        // 입력 처리
-        _movement.x = Input.GetAxis("Horizontal");
-        _movement.y = Input.GetAxis("Vertical");
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 
     private void FixedUpdate()
     {
+        // 입력 처리
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.y = Input.GetAxis("Vertical");
+
         // 이동 처리
         _rigidbody.MovePosition(_rigidbody.position + _movement * moveSpeed * Time.fixedDeltaTime);
+
+        // 애니메이션 처리
+        if (_movement.x != 0 || _movement.y != 0)
+        {
+            animator.SetBool("1_Move", true);
+
+            if (_movement.x > 0 && !_isRight)
+            {
+                Flip();
+                _isRight = true;
+            }
+            else if (_movement.x < 0 && _isRight)
+            {
+                Flip();
+                _isRight = false;
+            }
+        }
+        else
+        {
+            animator.SetBool("1_Move", false);
+        }
     }
 }
